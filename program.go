e1b5d39a48f1469635ad6d2fafc99348a2468f28
naithar01 <eeung025@gmail.com>
2024-03-30 14:24:48 +0900

    Edit Program Package

--- program.go
+++ program.go
@@ -1,10 +1,11 @@
-// Check Docker Program
+// Check Docker installed on PC OR Any other Programs
 
-package progream
+package godockerinfofetcher
+
+import "fmt"
 
 // PC에 Docker가 설치되어 있는지 확인
 func CheckDocker() bool {
-	// TOOD: Check Docker
-
+	fmt.Println("Test")
 	return true
 }
